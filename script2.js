6356e6c8dc46032f0f66be7589d79e3d50114017
Sigit Wahyudi <wahyudi.sigit@gmail.com>
2020-05-14 14:38:37 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         var data=JSON.parse(xhttp.responseText);
+         data.forEach(function(element) {
+      document.getElementById("tempat").innerHTML +="nama :" + element.nama + "<br>Alamat :" + element.alamat + "<br>Alamat :" + element.alamat + "<br><br>";
+    });
+      }
+  };
+  xhttp.open("GET", "https://sigitwahyudi.web.id/ajax/php.php", true);
+  xhttp.send();
